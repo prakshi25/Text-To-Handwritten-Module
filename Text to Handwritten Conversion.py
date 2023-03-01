1ef5c48c0d6a59162051d83a63fe6a7e048bc1ae
Prakshi Raval <81760048+prakshi25@users.noreply.github.com>
2023-02-28 20:47:18 -0500

    Update Text to Handwritten Conversion.py

--- Text to Handwritten Conversion.py
+++ Text to Handwritten Conversion.py
@@ -1,16 +1,24 @@
 from PIL import Image
 
-txt = open("Machine Learning.txt")
+txt = input('Enter the text you want to convert into handwritten notes: ')
 BG = Image.open("Letters/bg.png")
 sheet_width = BG.width
 gap, ht = 0, 0
 
 
-for i in txt.read().replace("\n",""):
-        cases = Image.open("Letters/{}.png".format(str(ord(i))))
+for i in txt:
+    if i in [" ", ".", ",", ":", ";"]:
+        gap += size // 2
+    elif i == "\n":
+        gap, ht = 0, ht + 140
+    else:
+        try:
+            cases = Image.open("Letters/{}.png".format(str(ord(i.lower()))))
+        except FileNotFoundError:
+            print("Error: Please write a short paragraph.", i)
+            continue
         BG.paste(cases, (gap, ht))
         size = cases.width
-        height = cases.height
         gap += size
 
         if sheet_width < gap or len(i)*115 >(sheet_width-gap):
